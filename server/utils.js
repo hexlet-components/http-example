62e55d25b5bb30bcc6e6f34d14589b2d669071be
Ivan Gagarinov <dzencot@gmail.com>
2024-09-16 17:20:37 +0500

    add fixtures

--- server/utils.js
+++ server/utils.js
@@ -1,5 +1,8 @@
 import { faker } from '@faker-js/faker';
 import _ from 'lodash';
+import users from '../__fixtures__/users.json' with { type: 'json' };
+import posts from '../__fixtures__/posts.json' with { type: 'json' };
+import comments from '../__fixtures__/comments.json' with { type: 'json' };
 
 const listSize = 10;
 const defaultLimit = 30;
@@ -28,27 +31,27 @@ const createComment = (post) => ({
 export const getToken = () => faker.string.alphanumeric(120);
 
 export const getInitData = () => {
-  const users = [];
+  // const users = [];
   
-  for(let i = 1; i <= listSize; i++) {
-    users.push(createUser());
-  }
+  // for(let i = 1; i <= listSize; i++) {
+  //   users.push(createUser());
+  // }
 
-  const posts = users.map((user) => {
-    const userPosts = [];
-    for(let i = 1; i <= listSize; i++) {
-      userPosts.push(createPost(user));
-    }
-    return userPosts;
-  }).flat();
+  // const posts = users.map((user) => {
+  //   const userPosts = [];
+  //   for(let i = 1; i <= listSize; i++) {
+  //     userPosts.push(createPost(user));
+  //   }
+  //   return userPosts;
+  // }).flat();
 
-  const comments = posts.map((post) => {
-    const postComments = [];
-    for(let i = 1; i <= listSize; i++) {
-      postComments.push(createComment(post));
-    }
-    return postComments;
-  }).flat();
+  // const comments = posts.map((post) => {
+  //   const postComments = [];
+  //   for(let i = 1; i <= listSize; i++) {
+  //     postComments.push(createComment(post));
+  //   }
+  //   return postComments;
+  // }).flat();
 
   return {
     users,
@@ -72,13 +75,14 @@ export const prepareListData = (name, state, context, callbackFilter = () => tru
   const skip = parseInt(context.request.query.skip ?? 0, 10)
   const limit = parseInt(context.request.query.limit ?? defaultLimit, 10);
   const { select } = context.request.query ?? [];
-  let list = state[name].filter(callbackFilter).slice(skip, skip + limit);
+  const totalList = state[name].filter(callbackFilter);
+  let list = totalList.slice(skip, skip + limit);
   if (select) {
     list = list.map((item) => prepareItem(item, select));
   }
   return {
     [name]: list,
-    total: list.length,
+    total: totalList.length,
     skip,
     limit,
   };
