af1b8c02397511762ad30cd15220f7177bb168f5
Ivan Gagarinov <dzencot@gmail.com>
2024-10-02 22:17:50 +0500

    fix

--- server/src/index.js
+++ server/src/index.js
@@ -147,7 +147,7 @@ export default async (app, _options) => {
   });
 
   app.get('/http-protocol/removed', (req, res) => {
-    reply.code(301).redirect('/http-protocol/cookies')
+    reply.code(301).redirect('/http-protocol/example');
   });
 
   app.get('/js-playwright/users-list', (req, res) => res.sendFile('users-list/index.html'));
